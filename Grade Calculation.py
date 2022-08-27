36ad12c728d381f308346f7b70c3dfaf83e09de8
Anila2580 <najiahossain133@gmail.com>
2022-08-27 11:47:16 +0600

    grade calculation

--- Grade Calculation.py
+++ Grade Calculation.py
@@ -1,25 +1,25 @@
+
 print ("Enter Marks: ")
 marks = int(input())
 
-if marks . > 0 and 32:
+if marks > 0 and 32:
     print('Invalid Marks')
 
 elif marks <33 and 40:
     print('Grade D')
-
-    elif marks < 41 and 49:
+elif marks < 41 and 49:
     print('Grade C')
 
-    elif marks < 50 and 59:
+elif marks < 50 and 59:
     print('Grade B')
 
-    elif marks < 60 and 69:
+elif marks < 60 and 69:
     print('Grade A-')
 
-    elif marks < 70 and 79:
+elif marks < 70 and 79:
     print('Grade A')
 
-    elif marks < 80 and 100:
+elif marks < 80 and 100:
     print('Grade A+')
 
 else:
---
+++ grading_system/grading_calcu.py
@@ -0,0 +1,29 @@
+print('enter marks')
+marks = int(input())
+
+if marks >=0 and marks <=32:
+    print("grade F")
+
+
+elif marks >= 33 and marks <=39:
+    print("grade D")
+
+elif marks >= 40 and marks <= 49:
+    print("grade C")
+
+elif marks >= 50 and marks <=59:
+    print("grade B")
+
+elif marks >= 60 and marks <=69:
+    print("grade A-")
+
+elif marks >= 70 and marks <=79:
+    print("grade A")
+
+elif marks >= 80 and marks <=100:
+    print("grade A+")
+
+else:
+    print("invalid marks")
+
+
