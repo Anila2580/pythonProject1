919dfb45b22833a079bf5ea8cab831fb227ac988
Anila2580 <najiahossain133@gmail.com>
2022-09-10 12:46:22 +0600

    inventory

---
+++ Inventory/checkInventory.py
@@ -0,0 +1,10 @@
+stuff = {'rope': 30,
+         'torch': 10,
+         'mobile': 20,
+         'sim card': 50
+         }
+print('Total quantity')
+print(sum(stuff.values()))
+print('total number of items')
+a = len(stuff)
+print(a)
